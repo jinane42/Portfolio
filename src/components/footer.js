94948e2b3d5168685ce1ede19347d73002387c96
Jinane42 <jinane42@hotmail.com>
2022-03-08 18:56:42 +0100

    first commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import '../styles/footer.css'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
